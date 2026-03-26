772b7bf133601787c0baf36cf7640c7496efb0d0
shahitha217-jpg <shahitha217@gmail.com>
2026-03-26 08:58:48 +0530

    Add files via upload

---
+++ UC7.java
@@ -0,0 +1,14 @@
+UC7
+
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(", ", args);
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
